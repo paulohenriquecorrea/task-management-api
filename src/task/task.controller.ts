88ede34a088bda11cd9d95e9d55b39bd70633192
unknown <ph.sistemas.ti@gmail.com>
2024-06-04 14:58:38 -0400

    Add Task Find By Id e Update

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,15 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { TaskDto } from './task.dto';
 import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
-    constructor(private readonly taskservice: TaskService) {
+  constructor(private readonly taskservice: TaskService) {}
 
-    }
+  @Post()
+  create(@Body() task: TaskDto) {
+    this.taskservice.create(task);
+  }
 
-    @Post()
-    create(@Body() task: TaskDto) {
-        this.taskservice.create(task);
-    }
+  @Get('/:id')
+  findById(@Param('id') id: string): TaskDto {
+    return this.taskservice.findById(id);
+  }
+
+  @Put()
+  update(@Body() task: TaskDto) {
+    this.taskservice.update(task);
+  }
 }
